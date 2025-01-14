45cf0f962de9e830c84d79fa0e2abe1fe90373ad
mhmd329 <mhmdkhaled092@gmail.com>
2025-01-14 15:20:40 +0200

    add buy button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import SportCard from './components/cards/sportCard';
-import Home from './components/home/Home';
-import NavBar from './components/nav/NavBar';
+import "./App.css";
+import SportCard from "./components/cards/sportCard";
+import Home from "./components/home/Home";
+import NavBar from "./components/nav/NavBar";
 
 function App() {
   return (
-    <div >
-      <NavBar/>
-      <Home/>
-      <SportCard/>
+    <div>
+      <NavBar />
+      <Home />
+      <SportCard />
     </div>
   );
 }
